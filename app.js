25e218c1dfac0bb29d0d5be02ab3a9f5d562c072
Josh Murtha <murthajosh@gmail.com>
2022-03-29 23:24:17 -0500

    wow it only took a few lines of code to get the copy to clipboard button working.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const button = document.querySelector('button')
 const phraseDisplay = document.querySelector('.phrase-display')
+const copyButton = document.querySelector('.button-copy')
 
+// gets and displays the text
 button.addEventListener('click', async () => {
     let response = await axios.get("https://corporatebs-generator.sameerkumar.website/")
     phraseDisplay.innerHTML = ''
     phraseDisplay.append(response.data.phrase)
+})
+
+copyButton.addEventListener('click', () => {
+    navigator.clipboard.writeText(phraseDisplay.innerHTML);
 })
